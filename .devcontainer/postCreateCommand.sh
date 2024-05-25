8d34b203b80c0c9682ae3eeb2c4ab9e15b9cb25f
cndoit18 <cndoit18@outlook.com>
2024-05-25 13:42:06 +0800

    add depth

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git submodule update --init --recursive
+git submodule update --init --recursive --depth=20
 rustup override set $(`dirname $0`/../linux_raspberrypi/scripts/min-tool-version.sh rustc)
 rustup component add rust-src
 cargo install --locked --version $(`dirname $0`/../linux_raspberrypi/scripts/min-tool-version.sh bindgen) bindgen-cli
