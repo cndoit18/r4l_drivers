c36a12f6fc570d1d68758db487645a1a3f9e53aa
cndoit18 <cndoit18@outlook.com>
2024-05-25 12:53:39 +0800

    update ignore

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+git submodule update --init --recursive
 rustup override set $(`dirname $0`/../linux_raspberrypi/scripts/min-tool-version.sh rustc)
 rustup component add rust-src
 cargo install --locked --version $(`dirname $0`/../linux_raspberrypi/scripts/min-tool-version.sh bindgen) bindgen-cli
